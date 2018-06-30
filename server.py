c104d2e3b22d68fa4750414102c79ed699fd123b
OpaxZity <40703395+OpaxZity@users.noreply.github.com>
2018-06-30 13:35:12 +0300

    Update server.py

--- server.py
+++ server.py
@@ -6,10 +6,20 @@ s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
+def imagenamechanger(imagename):
+   return imagename
+def imagetobytes(imagename):
+   with open(imagename, "rb") as imageFile:
+      f = imageFile.read()
+      b = bytearray(f)
+      return b
 
 s.listen(5)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
    print ('Got connection from', addr)
-   c.send(bytes('Sending bytes to client.py', 'utf-8'))
+   c.send(bytes(imagetobytes("fire.png")))
+
+
+
    c.close()                # Close the connection
