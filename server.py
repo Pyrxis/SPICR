49dec987a44d6eb47ae322a7d122ad91810d2611
OpaxZity <40703395+OpaxZity@users.noreply.github.com>
2018-07-03 17:53:35 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,25 +1,55 @@
-#!/usr/bin/python           # This is server.py file
-import io
-import socket               # Import socket module
+#basic folder/img information:
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
-def imagenamechanger(imagename):
-   return imagename
-def imagetobytes(imagename):
+#Original images are in Desktop/Pictures
+#Cloned images will be saved in Desktop/result
+#10 images in /Pictures numbered fire1.png ~ fire10.png
+#10 images will be in /result numbered newfire0.png ~ newfire9.png
+
+#what needs to be fixed
+
+#currently, it creates a "result" folder with 10 images labled "newfire0" ~ "newfire9", however,
+#only newfire0 is "loaded" meaning it is the only openable img that is cloned from the first picture
+#in the original /Pictures folder. The others are just image files without any data
+
+
+import io
+import socket               
+import os
+import sys
+
+
+num=1
+done = False
+s = socket.socket()
+a = b""
+host = socket.gethostname()
+port = 12345
+s.bind((host, port))
+
+
+def imagetobyte(imagename):     #This func takes in original img and returns bytearray of it
    with open(imagename, "rb") as imageFile:
       f = imageFile.read()
       b = bytearray(f)
       return b
 
-s.listen(5)                 # Now wait for client connection.
+
+
+s.listen(5)                
+
 while True:
-   c, addr = s.accept()     # Establish connection with client.
+   c, addr= s.accept()
+
+   
+   
    print ('Got connection from', addr)
-   c.send(bytes(imagetobytes("fire.png")))
+   numfiles = len([t for t in os.listdir('./Picture')]) #this /Picture is dir that contains initial images
 
-
-
-   c.close()                # Close the connection
+   while(num<=numfiles):
+      a += (imagetobyte('.\\Picture\\fire'+str(num)+'.png'))
+      num+=1
+      
+   c.send(bytes(a)) # if i put this line inside the while loop, none of the images get loaded. 
+      
+   
+   c.close()
