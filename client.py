c6f84d6728555874a245f7c35f23dfbcf1428b88
OpaxZity <40703395+OpaxZity@users.noreply.github.com>
2018-07-04 18:50:22 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,53 +1,36 @@
-#basic folder/img information:
-
-#Original images are in Desktop/Pictures
-#Cloned images will be saved in Desktop/result
-#10 images in /Pictures numbered fire1.png ~ fire10.png
-#10 images will be in /result numbered newfire0.png ~ newfire9.png
-
-#what needs to be fixed
-
-#currently, it creates a "result" folder with 10 images labled "newfire0" ~ "newfire9", however,
-#only newfire0 is "loaded" meaning it is the only openable img that is cloned from the first picture
-#in the original /Pictures folder. The others are just image files without any data
-
-
 import io
-import socket
+import socket,pickle
 import os
 import sys
 
 
-num=1
-done = False
 s = socket.socket()
-a = b""
+num = 0
 host = socket.gethostname()
 port = 12345
-s.bind((host, port))
+newpath = r'C:\Users\hajun_dg9ntlr\Desktop\result'
+s.connect((host, port))
 
 
-def imagetobyte(imagename):     #This func takes in original img and returns bytearray of it
-   with open(imagename, "rb") as imageFilex:
-      f = imageFilex.read()
-      b = bytearray(f)
-      return b
+try:     #tries to make folder result, if it can't (exists) then pass
+    os.makedirs(newpath)
+except:
+    pass
+
+numfiles = len([t for t in os.listdir('./Picture')]) #this /Picture is dir that contains initial images
+bytearr = b''
+datax = s.recv(9999999)
+dataofimg = pickle.loads(datax)
+while (num <= numfiles-1):
+    
+    f = open("result\\newfire"+str(num)+".png", "wb")
+    bytearr = bytearray(dataofimg[int(num)])
+    f.write(bytearr)
+    num+=1
 
 
 
-s.listen(5)                
 
-while True:
-   c, addr= s.accept() 
-   print ('Got connection from', addr)
+f.close
 
-
-   numfiles = len([t for t in os.listdir('./Picture')]) #this /Picture is dir that contains initial images
-
-   while(num<=numfiles):
-      a = b"" + (imagetobyte('.\\Picture\\fire'+str(num)+'.png'))
-      num+=1
-      c.send(a)
-   
-   
-   c.close()  
+s.close
