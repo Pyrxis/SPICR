5842f118c3385c5a117b505a43dfb454ad167f13
Pyrxis <oasishaon@gmail.com>
2018-08-01 09:53:37 +0000

    Update server.py

--- server.py
+++ server.py
@@ -11,22 +11,24 @@
 #currently, it creates a "result" folder with 10 images labled "newfire0" ~ "newfire9", however,
 #only newfire0 is "loaded" meaning it is the only openable img that is cloned from the first picture
 #in the original /Pictures folder. The others are just image files without any data
-
-
 import io
 import socket,pickle
 import os
 import sys
 import re
+import signal
 
-
-
+def handler(signum,frame): #for detecting when 1 minute has passed
+    print("TIMES UP!!!! 2 MINUTE ALREADY PASSED! exiting now....")
+    s.close()
+    sys.exit()
+    
 done = False
 s = socket.socket()
 
 host = socket.gethostname()
 port = 12345
-s.bind((host, port))
+s.bind(('', port))
 
 
 def imagetobyte(imagename):     #This func takes in original img and returns bytearray of it
@@ -38,7 +40,8 @@ def imagetobyte(imagename):     #This func takes in original img and returns byt
 
 
 s.listen(5)                
-
+signal.signal(signal.SIGALRM,handler)
+signal.alarm(60)
 while True:
    c, addr= s.accept() 
    print ('Got connection from', addr)
@@ -60,19 +63,22 @@ while True:
    a.append(file_no_safe)
    a.append(file_no_problem)
    for i in file_no_safe:
-      if(os.path.isfile('./result/safe/panel'+str(i)+'.png')):
+      if(os.path.isfile('./result/safe/panel'+str(i)+'.jpg')):
  
-         a.append( b"" + (imagetobyte('./result/safe/panel'+str(i)+'.png')))
+         a.append( b"" + (imagetobyte('./result/safe/panel'+str(i)+'.jpg')))
       else:
          pass
    for i in file_no_problem:
-      if(os.path.isfile('./result/problem/panel'+str(i)+'.png')):
-         a.append( b"" + (imagetobyte('./result/problem/panel'+str(i)+'.png')))
+      if(os.path.isfile('./result/problem/panel'+str(i)+'.jpg')):
+         a.append( b"" + (imagetobyte('./result/problem/panel'+str(i)+'.jpg')))
       else:
          pass
 
    print(type(a))
+   print(len(a))
    c.sendall(pickle.dumps(a))
    
    
    c.close()
+
+
